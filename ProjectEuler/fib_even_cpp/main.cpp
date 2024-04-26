70104a794320604706aef5d8a337425cad6d8bd3
Hideki A. Ikeda <hidekiai@CodeMonkeyNinja.dev@users.noreply.github.com>
2024-04-26 00:05:53 -0500

    doing both rust and C++ solutions in preparations for C++ tests

---
+++ ProjectEuler/fib_even_cpp/main.cpp
@@ -0,0 +1,33 @@
+// By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+
+#include <iostream> // cout
+
+
+const int MAX = 4000000;
+
+// recursive function for fibonacci
+int fib_rec(int n) {
+    if (n == 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else {
+        return fib_rec(n - 1) + fib_rec(n - 2);
+    }
+}
+
+int main() {
+    auto sum = 0;
+    auto i = 0;
+    while (true) {
+        auto fib = fib_rec(i);
+        if (fib > MAX) {
+            break;
+        }
+        if (fib % 2 == 0) {
+            sum += fib;
+        }
+        i++;
+    }
+    std::cout << "Sum = " << sum << std::endl;
+}
