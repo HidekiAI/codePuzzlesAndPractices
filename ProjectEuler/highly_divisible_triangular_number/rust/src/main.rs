5c1f6a628ab7cbdec7202f4aaa6be479955cc386
Hideki A. Ikeda <hidekiai@CodeMonkeyNinja.dev@users.noreply.github.com>
2024-04-27 09:00:03 -0500

    got a bit too obsessed on prime number and lost practice time

--- ProjectEuler/highly_divisible_triangular_number/rust/src/main.rs
+++ ProjectEuler/highly_divisible_triangular_number/rust/src/main.rs
@@ -30,12 +30,179 @@
 //   in which, I want to integrate and see how much better it performs.
 // * Overall, this problem took over 2hrs to solve (it is at 2:25:10 mark) and still waiting
 //   for the final answer.
+//use std::cell::OnceCell;
+//use lazy_static::lazy_static;
 
-const DIVISOR_COUNT: usize = 500;   // Triangle number 76576500 has 576 divisors.  Total time: 2:49:34
+const DIVISOR_COUNT: usize = 500; // Triangle number 76576500 has 576 divisors.  Total time: 2:49:34
+
+// embed binary file "../make_prime_table/prime_table_64k.bin" so that we can do quick prime number check
+// NOTE: 76576500 will fit if prime number lookup is up to 2^27 (134,217,728)
+const PRIME_TABLE_64K: &'static [u8] =
+    include_bytes!("../../../make_prime_table/prime_table_65535.bin");
+
+// Q: Can I read in TEXT file as a static array using include_str!()?
+const KNOWN_PRIMES_RAW: &'static str =
+    include_str!("../../../make_prime_table/known_primes_65535.txt");
+fn get_known_primes() -> Vec<&'static str> {
+    let lines: Vec<_> = KNOWN_PRIMES_RAW.lines().collect::<Vec<_>>();
+    //for i in 0..10 { println!("{}: {}", i, lines[i]); }
+    lines
+}
+//static KNOWN_PRIMES: Lazy<[&str; 65535]> = Lazy::new(|| {
+//    KNOWN_PRIMES_RAW
+//        .lines()
+//        .collect::<Vec<_>>()
+//        .try_into() // convert Vec<String> to [&str; 65535]
+//        .unwrap()
+//});
+//static KNOWN_PRIMES: OnceCell<[&'static str; 65535]> = OnceCell::new();
+//fn initialize_known_primes() {
+//    KNOWN_PRIMES.get_or_init(|| {
+//        KNOWN_PRIMES_RAW
+//            .lines()
+//            .collect::<Vec<_>>()
+//            .try_into()
+//            .unwrap()
+//    });
+//}
+//lazy_static! {
+//    static ref KNOWN_PRIMES: [&'static str; 65535] = {
+//        let lines: Vec<&str> = KNOWN_PRIMES_RAW.lines().collect();
+//        lines.try_into().unwrap()
+//    };
+//}
+
+// Prime number: a number greater than 1 that has no positive divisors other than 1 and itself.
+// i.e. 5, 7, 13, and 29 are prime numbers
+// Some rules:
+// * 1 is not a prime.
+// * All primes except 2 are odd.
+// * All primes greater than 3 can be written in the form 6k+/-1.
+// * Any number n can have only one primefactor greater than n .
+// * The consequence for primality testing of a number n is: if we cannot find a number f less than
+//   or equal n that divides n then n is prime: the only primefactor of n is n itself
+fn is_prime(n: usize) -> bool {
+    // 2 is a known prime number
+    if n == 2 {
+        return true;
+    }
+    // all primes except for 2 are odd
+    // 1 is not a prime
+    // if divisible by 2, it's not a prime number, also 0 and 1 are not prime numbers
+    if n < 2 || n % 2 == 0 {
+        return false;
+    }
+
+    if n > 3 {
+        // all primes greater than 3 can be written in the form 6k+/-1
+        if n % 6 != 1 && n % 6 != 5 {
+            return false;
+        }
+    }
+
+    // n < 9: 2, 3, 5, 7 (4, 6, and 8 tested above)
+    if n >= 9 && n % 3 == 0 {
+        return false;
+    }
+
+    // if n < 64K, we can just use the lookup table
+    if n < 65536 {
+        return PRIME_TABLE_64K[n] == 1;
+    }
+
+    let sqrt_n = ((n as f64).sqrt()).floor() as usize;
+    let mut f = 5;
+    while f <= sqrt_n {
+        if n % f == 0 {
+            return false;
+        }
+        if n % (f + 2) == 0 {
+            return false;
+        }
+        f += 6;
+    }
+
+    // if n = 13, the largest divisor is 13, which is less than sqrt(13) = 3.6
+    // if n = 25, the largest divisor is 5, which is less than sqrt(25) = 5
+    // if n = 29, the largest divisor is 29, which is less than sqrt(29) = 5.4
+    // hence if n is not a prime number, it must have a divisor less than its square root
+    // so for 25 = 5 * 5, the largest divisor is 5, which is less than sqrt(25) = 5
+    // for 29 = 29 * 1, the largest divisor is 29, which is less than sqrt(29) = 5.4
+    // If a number n has a divisor greater than its square root, it must also have a corresponding
+    // divisor smaller than its square root. Therefore, we only need to check up to the square root.
+    let mut i = 3; // we've already tested for 0..2, so start from 3
+    while i * i <= n {
+        if n % i == 0 {
+            return false; // opt out early
+        }
+        i += 1; // can I do +2 here?
+    }
+    // passed all the tests between 2 and sqrt(n), this number is prime
+    return true;
+}
 
 fn triangle_number(n: usize) -> usize {
     n * (n + 1) / 2
 }
+
+// N = P1^a * P2^b * P3^c * ... * Pn^z
+// where p1, p2, p3, ..., pn are prime numbers
+// For example, 28 = 2^2 * 7^1
+// D(N) = (a+1) * (b+1) * (c+1) * ... * (z+1)
+// D(28) = (2+1) * (1+1) = 3 * 2 = 6
+fn dn() -> usize {
+    let KNOWN_PRIMES = get_known_primes();
+    let mut n = 3;
+    let mut Dn = 2;
+    let mut cnt = 0;
+    let mut n1 = 0;
+    let mut Dn1 = 0;
+    let mut exponent = 0;
+    let mut current_prime_table_index: usize = 0;
+
+    while cnt <= DIVISOR_COUNT {
+        n += 1;
+        n1 = n;
+        if n1 % 2 == 0 {
+            n1 /= 2;
+        }
+        Dn1 = 1;
+
+        for i in 0..KNOWN_PRIMES.len() {
+            current_prime_table_index = i;
+            // read str and convert it to uint32
+            let prime = KNOWN_PRIMES[current_prime_table_index]
+                .parse::<usize>()
+                .unwrap();
+            if prime * prime > n1 {
+                Dn1 *= 2;
+                break;
+            }
+        }
+        // When the prime divisor would be greater than the residual n1,
+        // that residual n1 is the last prime factor with an exponent=1
+        // No necessity to identify it.
+        exponent = 1;
+        let prime = KNOWN_PRIMES[current_prime_table_index]
+            .parse::<usize>()
+            .unwrap();
+        while n1 % prime == 0 {
+            exponent += 1;
+            n1 /= prime;
+        }
+        if exponent > 1 {
+            Dn1 *= exponent;
+        }
+        if n1 == 1 {
+            break;
+        }
+        cnt = Dn * Dn1;
+        Dn = Dn1;
+    }
+
+    n * (n - 1) / 2
+}
+
 fn factors(n: usize) -> Vec<usize> {
     let mut factors = Vec::new();
     let mut factor_count = 0;
@@ -59,13 +226,15 @@ fn get_factor_count(n: usize) -> usize {
 }
 
 fn main() {
-    let mut n = 12300;  // NOTE: I already know that at 11900, Triangle=1707120 with 480 factor_count
+    // let mut n = 12300;  // NOTE: I already know that at 11900, Triangle=1707120 with 480 factor_count
+    let mut n = 0; //
     let mut largest_so_far = 0;
     let mut largest_so_far_triangle = 0;
     let mut avg_time_msec = 0.0;
     loop {
         let start_timer = std::time::Instant::now();
         let triangle = triangle_number(n);
+        //let factor_count = get_factor_count(triangle);
         let factor_count = get_factor_count(triangle);
         if factor_count > largest_so_far {
             largest_so_far = factor_count;
@@ -86,9 +255,8 @@ fn main() {
         if n % 100 == 0 {
             println!("Avg {avg_time_msec} mSec: Checking triangle number [{}..{}], max so far {largest_so_far} for Triangle={largest_so_far_triangle}", n, n + 100);
         }
-        avg_time_msec =  (avg_time_msec + start_timer.elapsed().as_millis() as f64) / 2.0;
+        avg_time_msec = (avg_time_msec + start_timer.elapsed().as_millis() as f64) / 2.0;
     }
-
 }
 
 #[cfg(test)]
@@ -128,5 +296,28 @@ mod tests {
 
         assert_eq!(factors(28), vec![1, 2, 4, 7, 14, 28]);
         assert_eq!(get_factor_count(28), 6);
+    }
+
+    #[test]
+    fn test_known_prime_table() {
+        // check to see if some known prime number is
+        // found in the prime_table_64k.bin and known_primes_64k.txt
+        let my_prime = 29;
+        assert_eq!(is_prime(my_prime), true);
+        println!("{} is a prime number", my_prime);
+
+        // check if it's in the prime_table_64k.bin
+        assert_eq!(PRIME_TABLE_64K[my_prime], 1);
+        println!("{} is in the prime_table_64k.bin", my_prime);
+
+        let lines: Vec<_> = KNOWN_PRIMES_RAW.lines().collect::<Vec<_>>();
+        for i in 0..10 {
+            println!("{}: {}", i, lines[i]);
+        }
+        // check if it's in the known_primes_64k.txt
+        let KNOWN_PRIMES = get_known_primes();
+        let found = KNOWN_PRIMES.iter().any(|&x| x == my_prime.to_string());
+        assert_eq!(found, true);
+        println!("{} is in the known_primes_64k.txt", my_prime);
     }
 }
--- ProjectEuler/make_prime_table/src/main.rs
+++ ProjectEuler/make_prime_table/src/main.rs
@@ -22,21 +22,73 @@ fn sieve_of_eratosthenes() {}
 
 fn trial_division() {}
 
+// Prime number: a number greater than 1 that has no positive divisors other than 1 and itself.
+// i.e. 5, 7, 13, and 29 are prime numbers
+// Some rules:
+// * 1 is not a prime.
+// * All primes except 2 are odd.
+// * All primes greater than 3 can be written in the form 6k+/-1.
+// * Any number n can have only one primefactor greater than n .
+// * The consequence for primality testing of a number n is: if we cannot find a number f less than 
+//   or equal n that divides n then n is prime: the only primefactor of n is n itself
 fn is_prime(n: usize) -> bool {
-    if n < 2 {
+    // 2 is a known prime number
+    if n == 2 {
+        return true;
+    }
+    // all primes except for 2 are odd
+    // 1 is not a prime
+    // if divisible by 2, it's not a prime number, also 0 and 1 are not prime numbers
+    if n < 2 || n % 2 == 0 {
         return false;
     }
 
-    for i in 2..n {
-        if n % i == 0 {
+    if n > 3 {
+        // all primes greater than 3 can be written in the form 6k+/-1
+        if n % 6 != 1 && n % 6 != 5 {
             return false;
         }
     }
 
-    true
+    // n < 9: 2, 3, 5, 7 (4, 6, and 8 tested above)
+    if n >= 9 && n % 3 == 0 {
+        return false;
+    }
+
+    let sqrt_n = ((n as f64).sqrt()).floor() as usize;
+    let mut f = 5;
+    while f <= sqrt_n {
+        if n % f == 0 {
+            return false;
+        }
+        if n % (f + 2) == 0 {
+            return false;
+        }
+        f += 6;
+    }
+
+    // if n = 13, the largest divisor is 13, which is less than sqrt(13) = 3.6
+    // if n = 25, the largest divisor is 5, which is less than sqrt(25) = 5
+    // if n = 29, the largest divisor is 29, which is less than sqrt(29) = 5.4
+    // hence if n is not a prime number, it must have a divisor less than its square root
+    // so for 25 = 5 * 5, the largest divisor is 5, which is less than sqrt(25) = 5
+    // for 29 = 29 * 1, the largest divisor is 29, which is less than sqrt(29) = 5.4
+    // If a number n has a divisor greater than its square root, it must also have a corresponding
+    // divisor smaller than its square root. Therefore, we only need to check up to the square root.
+    let mut i = 3; // we've already tested for 0..2, so start from 3
+    while i * i <= n {
+        if n % i == 0 {
+            return false; // opt out early
+        }
+        i += 1; // can I do +2 here?
+    }
+    // passed all the tests between 2 and sqrt(n), this number is prime
+    return true;
 }
 
-fn make_prime_table(max_value: usize, filename: &str) {
+// NOTE: On a 64-bit machine, usize is 64-bit, while on a 32-bit host, usize is 32-bit.
+//       (usize is a size of a pointer)
+fn make_prime_table(max_value: usize, index_lookup_filename: &str, known_primes_filename: &str) {
     // create an output stream, output will be no-space separated
     // and each line will be up to 64 numbers, either 0 (if not)
     // prime, and 1 if it is.
@@ -44,11 +96,13 @@ fn make_prime_table(max_value: usize, filename: &str) {
     // (i.e. row=0, column=0) is 0th prime number, and so on.
     // NOTE: 0 and 1 are NOT prime numbers, so they will be
     // marked as 0.
-    let mut file = std::fs::File::create(filename).unwrap();
+    let mut file_lookup = std::fs::File::create(index_lookup_filename).unwrap();
+    let mut file_known_primes = std::fs::File::create(known_primes_filename).unwrap();  // each line is just a prime number
 
     // Note that if the last row does not have 64 numbers, then
     // it will continue to iterate until the last number.
     let max_iter = (max_value / 64) + 1; // number of rows
+    println!("max_value={}, max_iter={} rows", max_value, max_iter);
     for row in 0..max_iter {
         // create a vector of 0 and 1, where 0 means NOT prime
         // and 1 means prime.  Up to 64 numbers per line.
@@ -67,12 +121,42 @@ fn make_prime_table(max_value: usize, filename: &str) {
 
             // mark/set this column's bit as 1
             prime_row64[col] = 1u8;
+            // and write the prime number to the known prime file
+            file_known_primes.write_all(format!("{}\n", n).as_bytes()).unwrap();
         }
         // write the row to the file (no carriage return!)
-        file.write_all(prime_row64.as_slice()).unwrap();
+        file_lookup.write_all(prime_row64.as_slice()).unwrap();
     }
 }
 
 fn main() {
-    make_prime_table(65535, "prime_table_65535.bin");
+    let start_timer = std::time::Instant::now();
+    make_prime_table(2 << 15, "prime_table_65535.bin", "known_primes_65535.txt");
+    println!("Elapsed time: {:?}", start_timer.elapsed());
+
+    let start_timer = std::time::Instant::now();
+    make_prime_table(2 << 26, "prime_table_4M.bin", "known_primes_4M.txt");
+    println!("Elapsed time: {:?}", start_timer.elapsed());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_prime() {
+        assert_eq!(is_prime(0), false); // 0 is not a prime
+        assert_eq!(is_prime(1), false); // 1 is NOT a prime
+        assert_eq!(is_prime(2), true);  // all prime except for 2 are odd
+        assert_eq!(is_prime(3), true);
+        assert_eq!(is_prime(4), false);
+        assert_eq!(is_prime(5), true);
+        assert_eq!(is_prime(6), false);
+        assert_eq!(is_prime(7), true);
+        assert_eq!(is_prime(22), false);
+        assert_eq!(is_prime(32), false);
+        assert_eq!(is_prime(38), false);
+        assert_eq!(is_prime(42), false);
+        assert_eq!(is_prime(43), true);
+    }
 }
